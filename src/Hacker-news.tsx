4c353f541bae00c96b021c186389c5b8f5ca4271
Adrian.Zhang <jzhang026@gmail.com>
2023-04-03 01:14:12 +0800

    initialize

---
+++ src/Hacker-news.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+type Item = {
+    by: string;
+    id: number;
+    score: number;
+    title: string;
+    url: string;
+}
+export default async function HNews() {
+    let { data } = await axios.get<string[]>(
+        'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+    );
+
+    data = data.slice(0, 20);
+
+    const stories = await Promise.all(data.map(id => {
+        return axios.get<Item>(
+            `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+        ).then(res => res.data)
+            .catch((err) => {
+                console.error('Stories list fetching error');
+                console.error(err);
+                return {} as Item;
+            })
+    }));
+
+    return (
+        <div>
+            <h1>News YCombinator - React Server Component</h1>
+            {
+                stories.length === 0 ?
+                    <p>API returned empty response</p>
+                    :
+                    stories.map(story => {
+                        return (
+                            <a key={story.id} href={story.url}>
+                                <h2>{story.title}</h2>
+                                <p>{story.score}</p>
+                            </a>
+                        )
+                    })
+            }
+        </div>
+    )
+}
